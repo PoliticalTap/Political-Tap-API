bdbe2a311e82c15eba85dcb0fa5600ff98d0fb20
chaklahk <chaklahk@mail.uc.edu>
2020-10-26 21:54:38 -0400

    Votesmart API Functionality
    Added Votesmart API parser and created a getCandidateList endpoint
    
    Co-Authored-By: doughedd <55254751+doughedd@users.noreply.github.com>
    Co-Authored-By: chaklakl <26938342+chaklakl@users.noreply.github.com>
    Co-Authored-By: aholtcoder <37028717+aholtcoder@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from votesmart import votesmart as Votesmart
+import json
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/getCandidateList", methods=["GET"])
+def get_candidate_list():
+    zip = request.args.get("zip")
+    zip4 = request.args.get("zip4")
+    candidates = Votesmart.candidates.getByZip(zip, zip4)
+
+    output = {}
+
+    for candidate in candidates:
+
+        if (candidate.electionStatus != "Running"):
+            continue
+
+        election_office = candidate.electionOffice
+
+        # TODO: Do not include null running mate
+        candidate_obj = {
+            "candidate_id" : candidate.candidateId,
+            "name" : candidate.ballotName,
+            "party" : candidate.electionParties,
+            "running_mate" :
+            {
+                "candidate_id" : candidate.runningMateId,
+                "name" : candidate.runningMateName
+            }
+        }
+        
+        if (election_office in output.keys()):
+            output[election_office].append(candidate_obj)
+        else:
+            output[election_office] = [candidate_obj]
+
+    return json.dumps(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
