7dc8801406f032f603d115919c34f050a1ff4fdf
chaklahk <chaklahk@mail.uc.edu>
2020-11-03 15:02:26 -0500

    Updated app to support heroku deployment
    Updated app to support heroku deployment.
    Made changes for allowing CORS.
    Created a temporary endpoint with a testing response.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from flask import current_app
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 if (app.config["ENV"] == "production"):
     app.config.from_object("config.ProductionConfig")
--- main.py
+++ main.py
@@ -41,5 +41,117 @@ def get_candidate_list():
 
     return json.dumps(output)
 
+@app.route("/testCandidate", methods=["GET"])
+def test_get_candidate_list():
+    return {
+        "President": [
+            {
+                "candidate_id": "53279",
+                "name": "Joe Biden",
+                "party": "Democratic",
+                "running_mate": {
+                    "candidate_id": "120012",
+                    "name": "Kamala Devi Harris"
+                }
+            },
+            {
+                "candidate_id": "15723",
+                "name": "Donald J. Trump",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "34024",
+                    "name": "Mike Pence"
+                }
+            }
+        ],
+        "U.S. House": [
+            {
+                "candidate_id": "166760",
+                "name": "Warren Davidson",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "178848",
+                "name": "Vanessa Enoch",
+                "party": "Democratic",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            }
+        ],
+        "State House": [
+            {
+                "candidate_id": "179097",
+                "name": "Sara Carruthers",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "189460",
+                "name": "Jennifer L. Gross",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "189459",
+                "name": "Chuck Horn",
+                "party": "Democratic",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "189462",
+                "name": "Thomas Hall",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "189464",
+                "name": "Michelle E. Novak",
+                "party": "Democratic",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            }
+        ],
+        "State Senate": [
+            {
+                "candidate_id": "78001",
+                "name": "George F. Lang",
+                "party": "Republican",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            },
+            {
+                "candidate_id": "179098",
+                "name": "Kathy Wyenandt",
+                "party": "Democratic",
+                "running_mate": {
+                    "candidate_id": "",
+                    "name": ""
+                }
+            }
+        ]
+    }
+
 if __name__ == "__main__":
     app.run(debug=True)
